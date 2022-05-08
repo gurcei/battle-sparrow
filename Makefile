907da4c5e6c47bdbe46a24317bb341971f770b78
Gurce Isikyildiz <Gurce.Isikyildiz@resmed.com.au>
2022-05-08 23:40:03 +1000

    Added a nicer title page placeholder

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 bas:
 	./c1541.exe C:/Users/GurceI/AppData/Roaming/xemu-lgb/mega65/hdos/MEGA65.D81 -read "bs3" BS3.PRG
 	./petcat -65 -o BS3.BAS BS3.PRG
+
+bas2prg:
+	./petcat -w65 -o BS3.PRG BS3.BAS
+	./c1541.exe C:/Users/GurceI/AppData/Roaming/xemu-lgb/mega65/hdos/MEGA65.D81 -delete bs3 -write BS3.PRG "bs3"
--- dumper.py
+++ dumper.py
@@ -1,20 +1,21 @@
 import os
 
-x = 0
-y = 21
-w = 4
-h = 3
+x = 10
+y = 8
+w = 42
+h = 5
+fl = "title"
 
 skip = x + 80*y
 
 print("chars")
 for r in range(h):
-    os.system("hexdump -v --skip {} --length {} -e '\"$%04_ax: \"' -e '{}/1 \"%d, \"' -e '\"\\n\"' draft.bin'".format(skip, w, w))
+    os.system("hexdump -v --skip {} --length {} -e '\"data \"' -e '{}/1 \"%d,\"' -e '\"\\n\"' {}.bin'".format(skip, w, w, fl))
     skip = skip + 80
 
 skip = x + 80*y
 
 print("colour")
 for r in range(h):
-    os.system("hexdump -v --skip {} --length {} -e '\"$%04_ax: \"' -e '{}/1 \"%d, \"' -e '\"\\n\"' draft.clr'".format(skip, w, w))
+    os.system("hexdump -v --skip {} --length {} -e '\"data \"' -e '{}/1 \"%d,\"' -e '\"\\n\"' {}.clr'".format(skip, w, w, fl))
     skip = skip + 80
